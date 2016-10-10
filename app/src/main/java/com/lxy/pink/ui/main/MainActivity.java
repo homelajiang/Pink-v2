978f8c11ab7d6ee4505e1661089ea8dadccd331c
yuan <liangxiyuan3@163.com>
2016-10-10 23:00:54 +0800

    添加资源文件

--- app/src/main/java/com/lxy/pink/ui/main/MainActivity.java
+++ app/src/main/java/com/lxy/pink/ui/main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.lxy.pink.ui.main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.lxy.pink.R;
@@ -11,5 +13,12 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public static void Launch(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
     }
 }
