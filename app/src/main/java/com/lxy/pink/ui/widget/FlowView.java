2cc5ea094482f4127f3487657d7539fb9529e3a9
homelajiang <liangxiyuan3@163.com>
2016-11-21 17:31:11 +0800

    添加下雪视图

---
+++ app/src/main/java/com/lxy/pink/ui/widget/FlowView.java
@@ -0,0 +1,65 @@
+package com.lxy.pink.ui.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by homelajiang on 2016/11/21 0021.
+ */
+
+public class FlowView extends View {
+    private static final int NUM_SNOWFLAKES = 40;
+    private static final int DELAY = 5;
+
+    private SnowFlake[] snowFlakes;
+
+    public FlowView(Context context) {
+        super(context);
+    }
+
+    public FlowView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlowView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    protected void resize(int width, int height) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+        snowFlakes = new SnowFlake[NUM_SNOWFLAKES];
+        for (int i = 0; i < NUM_SNOWFLAKES; i++) {
+            snowFlakes[i] = SnowFlake.create(width, height, paint);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w != oldw || h != oldh) {
+            resize(w, h);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (SnowFlake snowFlake : snowFlakes) {
+            snowFlake.draw(canvas);
+        }
+        getHandler().postDelayed(runnable, DELAY);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            invalidate();
+        }
+    };
+}
