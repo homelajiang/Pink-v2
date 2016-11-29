8afff98fa9180311f8016918c849d522412f9aaa
homelajiang <liangxiyuan3@163.com>
2016-11-29 13:44:12 +0800

    ri

---
+++ app/src/main/java/com/lxy/pink/core/location/LocationContract.java
@@ -0,0 +1,19 @@
+package com.lxy.pink.core.location;
+
+import com.lxy.pink.ui.base.BasePresenter;
+import com.lxy.pink.ui.base.BaseView;
+
+/**
+ * Created by homelajiang on 2016/11/29 0029.
+ */
+
+public interface LocationContract {
+
+    interface Presenter extends BasePresenter{
+
+    }
+
+    interface View{
+
+    }
+}
---
+++ app/src/main/java/com/lxy/pink/core/location/LocationPresenter.java
@@ -0,0 +1,8 @@
+package com.lxy.pink.core.location;
+
+/**
+ * Created by homelajiang on 2016/11/29 0029.
+ */
+
+public class LocationPresenter {
+}
