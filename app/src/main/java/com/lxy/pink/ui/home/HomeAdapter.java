abb31a3d1907d7dccf2d61fe59928ca141d34d70
yuan <liangxiyuan3@163.com>
2017-01-02 22:45:27 +0800

    机械键盘好爽啊

--- app/src/main/java/com/lxy/pink/ui/home/HomeAdapter.java
+++ app/src/main/java/com/lxy/pink/ui/home/HomeAdapter.java
@@ -11,10 +11,12 @@ import com.lxy.pink.R;
 import com.lxy.pink.core.PinkService;
 import com.lxy.pink.core.PinkServiceContract;
 import com.lxy.pink.data.model.location.PinkLocation;
+import com.lxy.pink.data.model.music.PlayList;
 import com.lxy.pink.data.model.music.Song;
 import com.lxy.pink.data.model.todo.TodoList;
 import com.lxy.pink.data.model.weather.Weather;
 import com.lxy.pink.player.IPlayback;
+import com.lxy.pink.player.PlayMode;
 import com.lxy.pink.player.PlaybackService;
 import com.lxy.pink.ui.home.model.PinkCalendarModel_;
 import com.lxy.pink.ui.home.model.PinkMusicModel_;
@@ -28,7 +30,7 @@ import butterknife.ButterKnife;
  * Created by yuan on 2016/10/20.
  */
 
-public class HomeAdapter extends EpoxyAdapter implements PinkServiceContract.View, IPlayback.Callback {
+public class HomeAdapter extends EpoxyAdapter implements PinkServiceContract.View, IPlayback, IPlayback.Callback {
 
     private final Context context;
     private PinkServiceContract.Presenter presenter;
@@ -52,8 +54,6 @@ public class HomeAdapter extends EpoxyAdapter implements PinkServiceContract.Vie
 
     public void setMusicService(PlaybackService mPlayer) {
         this.mPlayer = mPlayer;
-        pinkMusicModel_ = new PinkMusicModel_();
-        addModel(pinkMusicModel_);
     }
 
     @Override
@@ -123,22 +123,151 @@ public class HomeAdapter extends EpoxyAdapter implements PinkServiceContract.Vie
 
     @Override
     public void onSwitchLast(@Nullable Song last) {
-
+        if (pinkMusicModel_ == null) {
+            pinkMusicModel_ = new PinkMusicModel_();
+            addModel(pinkMusicModel_);
+        } else {
+            pinkMusicModel_.onSwitchLast(last);
+        }
     }
 
     @Override
     public void onSwitchNext(@Nullable Song next) {
-
+        if (pinkMusicModel_ == null) {
+            pinkMusicModel_ = new PinkMusicModel_();
+            addModel(pinkMusicModel_);
+        } else {
+            pinkMusicModel_.onSwitchNext(next);
+        }
     }
 
     @Override
     public void onComplete(@Nullable Song next) {
-
+        if (pinkMusicModel_ == null) {
+            pinkMusicModel_ = new PinkMusicModel_();
+            addModel(pinkMusicModel_);
+        } else {
+            pinkMusicModel_.onComplete(next);
+        }
     }
 
     @Override
     public void onPlayStatusChanged(boolean isPlaying) {
+        if (pinkMusicModel_ == null) {
+            pinkMusicModel_ = new PinkMusicModel_();
+            addModel(pinkMusicModel_);
+        } else {
+            pinkMusicModel_.onPlayStatusChanged(isPlaying);
+        }
+    }
 
+    @Override
+    public void setPlayList(PlayList list) {
+        if (mPlayer != null)
+            mPlayer.setPlayList(list);
+    }
+
+    @Override
+    public boolean play() {
+        if (mPlayer != null)
+            return mPlayer.play();
+        return false;
+    }
+
+    @Override
+    public boolean play(PlayList list) {
+        if (mPlayer != null)
+            return mPlayer.play(list);
+        return false;
+    }
+
+    @Override
+    public boolean play(PlayList list, int startIndex) {
+        if (mPlayer != null)
+            return mPlayer.play(list, startIndex);
+        return false;
+    }
+
+    @Override
+    public boolean play(Song song) {
+        if (mPlayer != null)
+            return mPlayer.play(song);
+        return false;
+    }
+
+    @Override
+    public boolean playLast() {
+        if (mPlayer != null)
+            return mPlayer.playLast();
+        return false;
+    }
+
+    @Override
+    public boolean playNext() {
+        if (mPlayer != null)
+            return mPlayer.playNext();
+        return false;
+    }
+
+    @Override
+    public boolean pause() {
+        if (mPlayer != null)
+            return mPlayer.pause();
+        return false;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if (mPlayer != null)
+            return isPlaying();
+        return false;
+    }
+
+    @Override
+    public int getProgress() {
+        if (mPlayer != null)
+            return mPlayer.getProgress();
+        return 0;
+    }
+
+    @Override
+    public Song getPlayingSong() {
+        if (mPlayer != null)
+            return mPlayer.getPlayingSong();
+        return null;
+    }
+
+    @Override
+    public boolean seekTo(int progress) {
+        if (mPlayer != null)
+            return mPlayer.seekTo(progress);
+        return false;
+    }
+
+    @Override
+    public void setPlayMode(PlayMode playMode) {
+        if (mPlayer != null)
+            mPlayer.setPlayMode(playMode);
+    }
+
+    @Override
+    public void registerCallback(Callback callback) {
+//nothing to do
+    }
+
+    @Override
+    public void unregisterCallback(Callback callback) {
+//nothing to do
+    }
+
+    @Override
+    public void removeCallbacks() {
+//nothing to do
+    }
+
+    @Override
+    public void releasePlayer() {
+//nothing to do
     }
 
 
--- app/src/main/java/com/lxy/pink/ui/home/model/PinkMusicModel.java
+++ app/src/main/java/com/lxy/pink/ui/home/model/PinkMusicModel.java
@@ -1,16 +1,19 @@
 package com.lxy.pink.ui.home.model;
 
+import android.support.annotation.Nullable;
+
 import com.airbnb.epoxy.EpoxyAttribute;
 import com.airbnb.epoxy.EpoxyModel;
 import com.lxy.pink.R;
 import com.lxy.pink.data.model.music.Song;
+import com.lxy.pink.player.IPlayback;
 import com.lxy.pink.ui.home.view.PinkMusicView;
 
 /**
  * Created by homelajiang on 2016/12/23 0023.
  */
 
-public class PinkMusicModel extends EpoxyModel<PinkMusicView> {
+public class PinkMusicModel extends EpoxyModel<PinkMusicView> implements IPlayback.Callback{
 
     @EpoxyAttribute
     Song song;
@@ -28,5 +31,25 @@ public class PinkMusicModel extends EpoxyModel<PinkMusicView> {
     @Override
     public int getSpanSize(int totalSpanCount, int position, int itemCount) {
         return totalSpanCount;
+    }
+
+    @Override
+    public void onSwitchLast(@Nullable Song last) {
+
+    }
+
+    @Override
+    public void onSwitchNext(@Nullable Song next) {
+
+    }
+
+    @Override
+    public void onComplete(@Nullable Song next) {
+
+    }
+
+    @Override
+    public void onPlayStatusChanged(boolean isPlaying) {
+
     }
 }
